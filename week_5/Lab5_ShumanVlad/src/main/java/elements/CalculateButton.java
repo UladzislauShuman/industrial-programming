66e640f160d5044621fa2ed06f744dc294a6cab2
UladzislauLinux <shumanvlad56@gmail.com>
2024-10-07 14:10:03 +0300

    week6

---
+++ week_5/Lab5_ShumanVlad/src/main/java/elements/CalculateButton.java
@@ -0,0 +1,19 @@
+package elements;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculateButton extends JButton
+{
+    public CalculateButton() throws Exception
+    {
+        super("Calculate");
+
+    }
+
+    public void calculate() throws Exception
+    {
+
+    }
+}
